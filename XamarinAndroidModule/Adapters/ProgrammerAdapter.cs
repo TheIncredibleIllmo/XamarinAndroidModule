0761ebb3c42d9d9099b528e530d22fedcba8a595
Eduardo Medina <emedina@alset.com.mx>
2018-11-10 13:53:51 -0600

    -Adapter finished

--- XamarinAndroidModule/Adapters/ProgrammerAdapter.cs
+++ XamarinAndroidModule/Adapters/ProgrammerAdapter.cs
@@ -9,10 +9,51 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using XamarinAndroidModule.Models;
 
 namespace XamarinAndroidModule.Adapters
 {
-    class ProgrammerAdapter
+    public class ProgrammerAdapter : BaseAdapter<Programmer>
     {
+
+        #region FIELDS
+        private Activity _context;
+        private List<Programmer> _programmerLst;
+        #endregion
+
+        public ProgrammerAdapter(Activity context, List<Programmer> programmerLst)
+        {
+            _context = context;
+            _programmerLst = programmerLst;
+        }
+
+        #region ADAPTER_MEMBERS
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            Programmer programmer = _programmerLst[position];
+
+            var view = _context.LayoutInflater.Inflate(Resource.Layout.ProgrammerRow,parent,false);
+
+            var progImg = view.FindViewById<ImageView>(Resource.Id.progImg);
+            var progNameTxt = view.FindViewById<TextView>(Resource.Id.progNameTxt);
+            var progSpecialtyTxt = view.FindViewById<TextView>(Resource.Id.progSpecialtyTxt);
+
+            progNameTxt.Text = programmer.Name;
+            progSpecialtyTxt.Text = programmer.Specialty;
+
+            return view;
+        }
+        public override Programmer this[int position] => _programmerLst[position];
+
+        public override int Count => _programmerLst.Count;
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+        #endregion
+
+
+
     }
 }
--- XamarinAndroidModule/Models/Programmer.cs
+++ XamarinAndroidModule/Models/Programmer.cs
@@ -18,5 +18,11 @@ namespace XamarinAndroidModule.Models
         public string Name { get; set; }
         public string Specialty { get; set; }    
         public string ImageUrl { get; set; }
+        
+        public override string ToString()
+        {
+            return $"{Name} - {Specialty}";
+        }
+
     }
 }
